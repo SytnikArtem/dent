192ca45b0328b86ee81b604327efe830d0247f2e
Sytnik Artem <artemasitnik@gmail.com>
2019-06-06 17:35:44 +0300

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
 
-
+    $('.news-bottom-star').click(function(){
+       $(this).addClass('active').prevAll().addClass('active');
+       $(this).prevAll().addClass('active');
+       $(this).nextAll().removeClass('active');
+    });
 });
